b9b8fafc29ac887159b1cfc9b87e4eccf81847cd
Korneli Pingula <pingulak9@gmail.com>
2026-05-22 18:00:09 +0100

    add loggin etl pipeline

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -2,17 +2,30 @@ from ingestion.load_data import load_sales_data
 from transformation.clean_transform import transform_sales_data
 from validation.validate_data import validate_sales_data
 from loading.load_processed_data import load_processed_data
+from utils.logger import get_logger
+
+
+logger = get_logger()
 
 
 def run_pipeline():
-    print("Starting AWS ETL Pipeline")
+    logger.info("Starting AWS ETL Pipeline")
 
     load_sales_data()
-    transform_sales_data()
-    validate_sales_data()
-    load_processed_data()
+    logger.info("Ingestion completed")
 
-    print("AWS ETL Pipeline completed successfully")
+    transform_sales_data()
+    logger.info("Transformation completed")
+
+    validate_sales_data()
+    logger.info("Validation completed")
+
+    load_processed_data()
+    logger.info("Loading completed")
+
+    logger.info("AWS ETL Pipeline completed successfully")
+
+    print("Pipeline executed successfully")
 
 
 if __name__ == "__main__":
